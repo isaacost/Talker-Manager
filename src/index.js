23d0d5876e810462aaf77272b38271d7fb3f7be7
Isabela Costa <isa-hcosta@hotmail.com>
2023-01-06 16:53:51 -0300

    iniciando o projeto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const talkerRouter = require('./routers/talker.router');
 
 const app = express();
 app.use(express.json());
+app.use('/talker', talkerRouter);
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
---
+++ src/routers/talker.router.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+// const fs = require('fs').promises;
+// const { join } = require('path');
+
+const router = Router();
+// const PATH = join(__dirname, '../talker.json');
+
+module.exports = router;
